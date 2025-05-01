887e10db221f6e14387b871aa80580fcbf99e012
Viktoria Kryukova <newphoto2019@yandex.ru>
2025-05-01 19:31:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,177 @@
+const map = new maplibregl.Map({
+  container: "map",
+  style:
+    "https://raw.githubusercontent.com/gtitov/basemaps/refs/heads/master/positron-nolabels.json",
+  center: [80, 47],
+  zoom: 2,
+});
+
+function updateSelectedList() {
+  const features = map.queryRenderedFeatures({
+    layers: ["clusters"],
+  });
+
+  document.getElementById("list-selected").innerHTML =
+    "<h2>Сейчас на карте</h2>";
+
+  features.map((f) => {
+    if (f.properties.cluster) {
+      const clusterId = f.properties.cluster_id;
+      const pointCount = f.properties.point_count;
+      map
+        .getSource("cities")
+        .getClusterLeaves(clusterId, pointCount, 0)
+        .then((clusterFeatures) => {
+          clusterFeatures.map(
+            (feature) =>
+              (document.getElementById(
+                "list-selected"
+              ).innerHTML += `<div class="list-item">
+                        <h4>${feature.properties["Город"]}</h4>
+                        <a href='#' onclick="map.flyTo({center: [${f.geometry.coordinates}], zoom: 10})">Показать</a>
+                        </div><hr>`)
+          );
+        });
+    } else {
+      document.getElementById(
+        "list-selected"
+      ).innerHTML += `<div class="list-item">
+                    <h4>${f.properties["Город"]}</h4>
+                    <a href='#' onclick="map.flyTo({center: [${f.geometry.coordinates}], zoom: 10})">Показать</a>
+                    </div><hr>`;
+    }
+  });
+}
+
+map.on("load", () => {
+  map.addSource("russia-boundary", {
+    type: "geojson",
+    data: "./data/russia.geojson",
+  });
+
+  map.addLayer({
+    id: "russia-fill",
+    source: "russia-boundary",
+    type: "fill",
+    paint: {
+      "fill-color": "#dae0b8",
+      "fill-opacity": 0.3,
+    },
+    beforeId: "clusters",
+  });
+
+  fetch(
+    "https://docs.google.com/spreadsheets/d/1OcPpq0sigqPR0f4P5WAcHBdISJjzJ6kGBa0ZLRjZK-g/export?format=csv"
+  )
+    .then((response) => response.text())
+    .then((csv) => {
+      const rows = Papa.parse(csv, { header: true });
+      const geojsonFeatures = rows.data.map((row) => {
+        return {
+          type: "Feature",
+          properties: row,
+          geometry: {
+            type: "Point",
+            coordinates: [row.lon, row.lat],
+          },
+        };
+      });
+      const geojson = {
+        type: "FeatureCollection",
+        features: geojsonFeatures,
+      };
+
+      map.addSource("cities", {
+        type: "geojson",
+        data: geojson,
+        cluster: true,
+        clusterRadius: 20,
+      });
+
+      map.addLayer({
+        id: "clusters",
+        source: "cities",
+        type: "circle",
+        paint: {
+          "circle-color": [
+            "case",
+            ["has", "point_count"],
+            [
+              "step",
+              ["get", "point_count"],
+
+              "#74a892",
+              2,
+              "#f08441",
+              5,
+              "#f0daa5",
+            ],
+            "#74a892",
+          ],
+          "circle-stroke-width": 1,
+          "circle-stroke-color": "#FFFFFF",
+          "circle-radius": ["step", ["get", "point_count"], 12, 3, 20, 6, 30],
+        },
+      });
+
+      map.addLayer({
+        id: "clusters-labels",
+        type: "symbol",
+        source: "cities",
+        layout: {
+          "text-field": ["get", "point_count"],
+          "text-size": 10,
+        },
+      });
+
+      map.addLayer({
+        id: "unclustered-labels",
+        type: "symbol",
+        source: "cities",
+        filter: ["!", ["has", "point_count"]],
+        layout: {
+          "text-field": ["get", "Город"],
+          "text-size": 12,
+          "text-offset": [0, 0.75],
+          "text-anchor": "top",
+          "text-allow-overlap": false,
+        },
+        paint: {
+          "text-color": "#000000",
+          "text-halo-color": "#FFFFFF",
+          "text-halo-width": 1,
+        },
+      });
+
+      // add all items to list
+      geojson.features.map((f) => {
+        document.getElementById(
+          "list-all"
+        ).innerHTML += `<div class="list-item">
+                    <h4>${f.properties["Город"]}</h4>
+                    <a href='#' onclick="map.flyTo({center: [${f.geometry.coordinates}], zoom: 10})">Найти на карте</a>
+                    </div><hr>`;
+      });
+
+      // first update
+      map.once("idle", updateSelectedList);
+
+      // update when moving
+      map.on("moveend", () => {
+        map.once("idle", updateSelectedList);
+      });
+
+      // clusters
+      map.on("click", "clusters", function (e) {
+        map.flyTo({ center: e.lngLat, zoom: 8 });
+      });
+
+      map.on("mouseenter", "clusters", function () {
+        map.getCanvas().style.cursor = "pointer";
+      });
+
+      map.on("mouseleave", "clusters", function () {
+        map.getCanvas().style.cursor = "";
+      });
+    });
+});
